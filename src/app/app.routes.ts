bdb34646c10ade162c9ad22063832ebb05413e62
Your Name <you@example.com>
2024-10-01 19:36:13 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './page/login-page/login-page.component';
+import { SignInPageComponent } from './page/sign-in-page/sign-in-page.component';
+import { MovieListComponent } from './page/movie-list/movie-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: "",
+    component:LoginPageComponent
+},
+{
+    path: "signIn",
+    component:SignInPageComponent
+},
+{
+    path: "movieList",
+    component:MovieListComponent
+}
+];
---
+++ src/app/page/sign-in-page/sign-in-page.component.ts
@@ -0,0 +1,132 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { NgFor, NgIf } from '@angular/common';
+import { OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-in-page',
+  standalone: true,
+  imports: [FooterComponent,NgFor,FormsModule,NgIf],
+  templateUrl: './sign-in-page.component.html',
+  styleUrl: './sign-in-page.component.css'
+})
+export class SignInPageComponent implements OnInit{
+  public countrys:any=[];
+
+  public txtEmailV:String=""
+  public txtUserNameV:String=""
+  public txtPasswordV:String=""
+  public txtCountryV:String=""
+
+  public textCName:String="";
+  public userInfo:any={
+    email:"",
+    userName:"",
+    password:"",
+    country:""
+
+  };
+
+  emailValidation():void{
+    if(!(/^\w{2,}@gmail.com$/.test(this.userInfo.email))){
+      this.txtEmailV="* Invalid email address"
+    }
+    else{
+      this.txtEmailV="";
+    }
+  }
+
+  userNameValidation():void{
+    if(this.userInfo.userName.trim().length<4){
+      this.txtUserNameV="* Length must be more than 3"
+   }
+   else{
+    this.txtUserNameV="";
+   }
+  }
+
+  passwordValidation():void{
+    let numbers=this.userInfo.password.match(/\d/g) || [];
+    let letters=this.userInfo.password.match(/[a-zA-Z]/g) || [];
+    if(numbers.length<4){
+      this.txtPasswordV="* Password atleast must contain 4 numbers"
+    }
+    else if(letters.length<4){
+      this.txtPasswordV="* Password atleast must contain 4 letters"
+    }
+
+    else{
+      this.txtPasswordV="";
+    }
+  }
+
+  countryValidation():void{
+    let isValid=false;
+      this.countrys.forEach((e:any) => {
+        if(e.cName==this.userInfo.country){
+          isValid=true;
+        }
+      });
+ 
+      isValid ? this.txtCountryV="" : this.txtCountryV="* Country coudn't find";
+     
+  }
+
+  setName(cName:String):void{
+    this.userInfo.country=cName;
+  }
+
+  SignIn():void{
+      this.emailValidation();
+      this.userNameValidation();
+      this.passwordValidation();
+      this.countryValidation();
+      if(this.txtEmailV=="" && this.txtUserNameV=="" && this.txtPasswordV=="" && this.txtCountryV==""){
+        let header=new Headers;
+        header.append("Content-Type","application/json");
+        let body=JSON.stringify({
+            "email":this.userInfo.email,
+            "userName":this.userInfo.userName,
+            "password":this.userInfo.password,
+            "country":this.userInfo.country
+        });
+        const req={
+          method:"POST",
+          headers:header,
+          body:body
+        }
+        fetch("http://localhost:8080/add-Account",req)
+        .then(res=>res.json())
+        .then(data=>{
+          console.log(data);
+          if(data.emailDuplicate){
+            this.txtEmailV="* This email address already has been used"
+          }
+          if(data.userNameDuplicate){
+            this.txtUserNameV="* This user name already has been used"
+          }
+          
+        })
+        .catch((e:any)=>console.log("Error"+e));
+      }
+
+  }
+
+  ngOnInit():void{
+    fetch("https://restcountries.com/v3.1/all")
+    .then(res=>res.json())
+    .then(data=>{
+      data.forEach((e:any) => {
+        this.countrys.push({
+          cName: e.name.common,
+          cFlag:e.flags.png
+        }
+        );
+      });
+    })
+  }
+
+}
+
+
